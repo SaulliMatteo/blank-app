ea2777fc40f5dd1a95d3fe3f32f2b478b0c7677f
vitaleMorto <matteosaulli31@gmail.com>
2025-04-30 17:13:16 +0200

    modify stramlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 29 01:43:29 2025
+
+@author: Matteo
+"""
+
+import numpy as np
+import pickle
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+model = pickle.load(open('C:/Users/Matteo/Desktop/Informatica/Python/ML/diatabes_pred_trained.sav', 'rb'))
+
+def diatabes_pred(input_data):
+
+    data = np.asarray(input_data)
+    data = data.reshape(1, -1)
+    pred = model.predict(data)
+    
+    if(pred[0]):
+        return 'diabetic'
+    else:
+        return 'non diabetic'
+    
+def main():
+    st.title("diabetes pred Web App")
+    
+    # Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age,Outcome
+    Pregnancies = st.number_input('number of Pregnancies')
+    Glucose = st.number_input('number of Glucose')
+    BloodPressure = st.number_input('number of BloodPressure')
+    SkinThickness = st.number_input('number of SkinThickness')
+    Insulin = st.number_input('number of Insulin')
+    BMI = st.number_input('number of BMI')
+    DiabetesPedigreeFunction = st.number_input('number of DiabetesPedigreeFunction')
+    Age = st.number_input('number of Age')
+    
+    diagnosis = ''
+    datas = [Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]
+
+    if st.button('diabates result'):
+        diagnosis = diatabes_pred(datas)
+    
+    st.success(diagnosis)
+
+if __name__ == "__main__":
+    main()
